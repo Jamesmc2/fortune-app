7780a18fb993b07f73443ceac7e44034246cbc38
jimmy <jamesmc21998@gmail.com>
2023-07-28 11:54:55 -0500

    add fortune and lotto controllers

--- app/controllers/my_examples_controller.rb
+++ app/controllers/my_examples_controller.rb
@@ -1,2 +1,18 @@
 class MyExamplesController < ApplicationController
+  def fortune
+    prng = Random.new
+    fortunes = ["You will win the lottery", "You will go bankrupt", "You will live forever"]
+    random = prng.rand(0..2)
+    render json: { message: "#{fortunes[random]}" }
+  end
+
+  def lotto
+    prng = Random.new
+    winner = []
+    6.times do
+      winner << prng.rand(1..60)
+    end
+    p "The winning numbers are: #{winner}"
+    render json: { message: "The winning numbers are:  #{winner}" }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-
+  get "/fortune", controller: "my_examples", action: "fortune"
+  get "/lotto", controller: "my_examples", action: "lotto"
 end
